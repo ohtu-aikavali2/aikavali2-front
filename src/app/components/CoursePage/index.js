f6e475d0f6e8b012bcb5e2aced07704460611b75
woltsu <oltsuwh@gmail.com>
2018-11-10 00:43:51 +0200

    trying to fix travis

--- src/app/components/CoursePage/index.js
+++ src/app/components/CoursePage/index.js
@@ -13,7 +13,7 @@ export class CoursePage extends Component {
     const { courses } = this.props
     return (
       <div className='course-page'>
-        {courses !== undefined && courses.map(course => (
+        {courses && courses.map(course => (
           <Course key={course.name} title={course.name} />
         ))}
       </div>
