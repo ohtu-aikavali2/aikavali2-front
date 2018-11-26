da4a7fb17ad729ef0b85b0c699786bc3071f21f0
Matias Isosaari <matias_isosaari@hotmail.com>
2018-11-26 18:51:39 +0200

    removed public route for now

--- src/app/App.js
+++ src/app/App.js
@@ -41,7 +41,6 @@ export class App extends React.Component {
               <Route exact path='/' render={() => <Redirect to='/courses' />} />
               <Route exact path='/courses' render={({ history }) => <CoursePage history={history} />} />
               <Route exact path='/courses/:name' render={({ match }) => <FrontPage course={match.params.name} />} />
-              <Route exact path='/newquestion' render={({ history }) => <ConnectedQuestionForm history={history} />} />
               <ProtectedRoute
                 path='/admin'
                 redirectTo='/'
