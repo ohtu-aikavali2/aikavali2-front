d097a08727466eb7db87dea789158e2f75a2e1f3
woltsu <oltsuwh@gmail.com>
2018-12-12 11:51:23 +0200

    removed console.log

--- src/app/reducers/actions/groupActions.js
+++ src/app/reducers/actions/groupActions.js
@@ -4,7 +4,6 @@ import groupService from '../../services/groupService'
 export const createGroup = (data) => {
   return async (dispatch) => {
     const newGroup = await groupService.createGroup(data)
-    console.log('new', newGroup)
     dispatch({
       type: groupConstants.CREATE_GROUP_SUCCESSFUL,
       data: newGroup
