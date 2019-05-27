17e5cf12ff20abe90bd407fe0f650e228a6ab8b0
magael <mikael.jaakkola@helsinki.fi>
2019-05-27 21:14:08 +0300

    icons to indicate correct and incorrect answers

--- src/app/components/Question/QuestionAnswer.js
+++ src/app/components/Question/QuestionAnswer.js
@@ -58,6 +58,23 @@ export class QuestionAnswer extends Component {
     return null
   }
 
+  determineAnswerIcon = () => {
+    const { userAnswer, value, selected, correctAnswer } = this.props
+    const checkMark = 'M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M11,16.5L18,9.5L16.59,8.09L11,13.67L7.91,10.59L6.5,12L11,16.5Z'
+    const cross = 'M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z'
+    if ((userAnswer && userAnswer.isCorrect && selected) || correctAnswer === value) {
+      return checkMark
+    } else if (userAnswer && userAnswer.correctAnswer === value) {
+      return checkMark
+    } else if (selected && userAnswer && !userAnswer.isCorrect) {
+      return cross
+    } else if (selected && !userAnswer) {
+      return ''
+    } else if (userAnswer && userAnswer.correctAnswer !== value) {
+      return ''
+    }
+  }
+
   render() {
     const { classes, value, userAnswer, selected, answering, dumb } = this.props
     const style = this.determineStyle()
@@ -75,6 +92,9 @@ export class QuestionAnswer extends Component {
             <CardContent>
               <Grid container wrap="nowrap" spacing={16} className='containerGrid'>
                 <Grid style={textStyle} item className='itemGrid'>
+                  <svg style={{ width: '24px', height: '24px', viewBox: '0 0 24 24', float: 'left', padding: '10px' }}>
+                    <path fill="#000000" d={this.determineAnswerIcon()} />
+                  </svg>
                   <ReactMarkdown source={answer_lines} />
                 </Grid>
               </Grid>
@@ -82,7 +102,7 @@ export class QuestionAnswer extends Component {
             </CardContent>
           </CardActionArea>
         </Card>
-      </div>
+      </div >
     )
   }
 }
