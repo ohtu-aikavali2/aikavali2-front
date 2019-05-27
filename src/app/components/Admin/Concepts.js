0c324f8fd2a29aef242550cfcd45cf0cd85468d8
Olli Rousu <olli.rousu@hotmail.com>
2019-05-27 13:16:28 +0300

    concepts page shows courses

--- src/app/components/Admin/Concepts.js
+++ src/app/components/Admin/Concepts.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCourses} from '../../reducers/actions/courseActions'
+import ClickBox from '../common/ClickBox'
 
-class Concepts extends Component {
-  render () {
+class Courses extends Component {
+  state = {
+    step: 0,
+    concept: '',
+    selectedCourse: ''
+  }
+
+  componentDidMount() {
+    this.props.fetchCourses()
+  }
+
+  render() {
+    const step = this.state.step
+    const { courses } = this.props
+    console.log(courses)
     return (
       <div>
-        <p>Konseptit</p>
+        {step === 0 && (
+          <React.Fragment>
+            <h2>Valitse kurssi</h2>
+            {courses.map(course => <ClickBox key={course._id} title={course.name} onClick={() => console.log('painettu')}/>)}
+          </React.Fragment>
+        )}
       </div>
     )
   }
 }
 
-export default Concepts
+const mapStateToProps = (state) => ({
+  courses: state.course.courses
+})
+
+const mapDispatchToProps = {
+  fetchCourses
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Courses)
