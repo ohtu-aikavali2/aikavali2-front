cdfc96da0b95d90fd85a74e3fccdddc414926489
Olli Rousu <olli.rousu@hotmail.com>
2019-05-27 12:09:08 +0300

    new branch and added a manage concepts option to AdminUI

---
+++ src/app/components/Admin/Concepts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Concepts extends Component {
+  render () {
+    return (
+      <div>
+        <p>Konseptit</p>
+      </div>
+    )
+  }
+}
+
+export default Concepts
