752892f6225ea51b9db22b8dc3c9959a82d1664b
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2019-06-11 14:56:48 +0300

    rm unused style options

--- src/app/components/Question/QuestionAnswer.js
+++ src/app/components/Question/QuestionAnswer.js
@@ -36,7 +36,6 @@ export class QuestionAnswer extends Component {
     const selectedStyle = { backgroundColor: 'rgb(230, 243, 255)', cursor: 'default' }
     const correctStyle = { backgroundColor: 'rgb(113, 218, 113)' }
     const wrongStyle = { backgroundColor: 'rgb(255, 128, 128)', cursor: 'default' }
-    const notSelectedWrongStyle = { cursor: 'default' }
     const checkOutline = 'M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'
     const checkFilled = 'M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M11,16.5L18,9.5L16.59,8.09L11,13.67L7.91,10.59L6.5,12L11,16.5Z'
     const cross = 'M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z'
@@ -60,11 +59,6 @@ export class QuestionAnswer extends Component {
         backgroundStyle: selectedStyle,
         answerIcon: checkOutline
       }
-    } else if (userAnswer && userAnswer.correctAnswer && !userAnswer.correctAnswer.includes(value)) {
-      style = {
-        backgroundStyle: notSelectedWrongStyle,
-        answerIcon: ''
-      }
     }
     return style
   }
@@ -74,7 +68,7 @@ export class QuestionAnswer extends Component {
     const selected = selectedList ? (selectedList.map(s => s.value).includes(value)) : false
     const style = this.determineStyle(selected)
     const textStyle = {}
-    if (answering || (!selected && userAnswer && userAnswer.correctAnswer !== value)) {
+    if (answering || (!selected && userAnswer)) {
       textStyle['color'] = 'grey'
     }
     const answer_lines = '```\n' + value + ''
