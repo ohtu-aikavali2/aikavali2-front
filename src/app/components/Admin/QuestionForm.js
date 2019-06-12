ee44c5b55c796405f7fe6f4e8c2477768643f2c2
Mikko Leinonen <mikko.leinonen91@gmail.com>
2019-06-12 15:41:30 +0300

    removed console log

--- src/app/components/Admin/QuestionForm.js
+++ src/app/components/Admin/QuestionForm.js
@@ -158,7 +158,6 @@ export class QuestionForm extends Component {
           }
           return temp
         })
-        console.log('changedwhilelocked', changedCheckedStatus)
         this.setState({
           answerOptions: changedCheckedStatus
         })
